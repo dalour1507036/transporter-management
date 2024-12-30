95ef9a0a6878d47e329bb6d4a52e19c197d5b732
Saeed/ITP/Md. Dalour Hossan Saeed (Email: mddalour.h@reddotdigitalit.com) <MdDalour.H@reddotdigitalit.com>
2024-12-30 08:31:13 +0600

    first commit Introduce project skeleton

---
+++ src/main/java/com/tripsync/transporter_management/TransporterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tripsync.transporter_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporterManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripsync/transporter_management/TransporterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripsync.transporter_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
